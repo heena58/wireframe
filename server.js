6bbd28b219fde9c5a0d42a53ee6c33cd91a97cdb
heena58 <heenaroy58@gmail.com>
2018-07-22 00:19:06 -0400

    no message

---
+++ server.js
@@ -0,0 +1,24 @@
+var port = 1112,
+
+    express = require('express'),
+
+    app = express().use(express.static(__dirname + '/')),
+
+    http = require('http').Server(app);
+
+
+
+app.use('/', function (req, res) {
+
+    res.sendFile(__dirname + '/index.html');
+
+});
+
+
+
+http.listen(port, function () {
+
+    console.log("Node server listening on port " + port);
+
+});
+
